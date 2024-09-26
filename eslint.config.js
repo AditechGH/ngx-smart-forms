e5c20a70c8d4da261e79fdb7ee70a76c7f9db8fb
Adinan Alhassan <aditech5170@gmail.com>
2024-09-26 21:59:31 +0000

    feat(shared-utils): update shared-utils library to be buildable
    - Added a build target to the library's project.json for Nx build support
    - Created ng-package.json to configure the library build with ng-packagr
    - Updated tsconfig.lib.json to include necessary compiler options for building
    - Ensured the library is now buildable and can be distributed as a package

--- eslint.config.js
+++ eslint.config.js
@@ -40,4 +40,14 @@ module.exports = [
     },
     languageOptions: { parser: require('jsonc-eslint-parser') },
   },
+  {
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        { ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'] },
+      ],
+    },
+    languageOptions: { parser: require('jsonc-eslint-parser') },
+  },
 ];
--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/shared-utils/shared-utils.component';
+
+export * from './lib/utils';
