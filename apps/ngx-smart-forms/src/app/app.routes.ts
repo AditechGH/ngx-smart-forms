80ec97f1c93e7b05394deba1ce71618c5a359bda
Adinan Alhassan <aditech5170@gmail.com>
2024-09-29 19:02:08 +0000

    feat(app.routes): add route for SmartErrorDisplayDemoComponent

--- apps/ngx-smart-forms/src/app/app.routes.ts
+++ apps/ngx-smart-forms/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { SmartErrorDisplayDemoComponent } from './smart-error-display-demo/smart-error-display-demo.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'smart-error-display-demo',
+    component: SmartErrorDisplayDemoComponent,
+  },
+];
