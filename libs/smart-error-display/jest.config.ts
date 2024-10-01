9c2ca1d472936373e6984abbd0cc17a36885d0e7
Adinan Alhassan <aditech5170@gmail.com>
2024-10-01 06:16:39 +0000

    Move smart-error-display into libs directory (#2)

--- libs/smart-error-display/jest.config.ts
+++ libs/smart-error-display/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'smart-error-display',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../coverage/smart-error-display',
+  coverageDirectory: '../../coverage/smart-error-display',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
